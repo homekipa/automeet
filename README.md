5f204f1ee766566a9cfdfc96e4f7ca2e1375cf96
homekipa <68042207+homekipa@users.noreply.github.com>
2021-01-24 23:04:37 +0900

    Add files via upload

---
+++ README.md
@@ -0,0 +1,27 @@
+# Auto Meet
+
+**function**
+
+- access_meet()
+  google meet URL set & Mute/Camera off using hotkey() and "Request participation" button find, click!
+
+
+
+**User Should Do**
+
+- google meet URL set
+- TIME set / You have to set the time to request participation
+
+
+
+**Development  Environment**
+
+- python 3.8
+
+- webbrowser module
+
+  > pip install webbrowser
+
+- pyautogui module
+
+  > pip install pyautogui
---
+++ auto_meet.py
@@ -0,0 +1,50 @@
+import webbrowser
+import pyautogui
+import time
+import os
+import subprocess
+
+count_flag = 0
+
+def access_meet(num):
+	meet_url = [
+	"https://meet.google.com/xpv-mvyw-zcp",
+	"https://meet.google.com/uhk-nfbt-cae"
+	]
+	print(pyautogui.size())
+	os.startfile('chrome.exe')
+	time.sleep(3)
+
+	webbrowser.open(meet_url[num])
+	time.sleep(5)
+
+	# pyautogui.hotkey('ctrl', 'd')
+	# time.sleep(1)
+	pyautogui.hotkey('ctrl', 'e')
+
+	button_location = pyautogui.locateOnScreen('access.png')
+	time.sleep(1)
+	print('[DEBUG] Find Button : ',button_location)
+	pyautogui.click(button_location)
+
+def end_process():
+	os.system('taskkill /f /im chrome.exe')
+
+def start(start_hour, start_min, index, total_hour):
+	global count_flag
+
+	while True:
+		lecture_hour = int(time.strftime('%H'))
+		lecture_min = int(time.strftime('%M'))
+		if lecture_hour == start_hour and lecture_min == start_min and count_flag == index:
+			print('Try to Access...')
+			access_meet(count_flag)
+			count_flag += 1;
+
+		if lecture_hour == start_hour + total_hour:
+			end_process()
+			break
+
+# start(start_hour, start_min, index, total_hour)
+start(9,9,0,3)
+start(13,00,1,3)
